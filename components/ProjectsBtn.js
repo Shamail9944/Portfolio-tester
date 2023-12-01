8e4587cd5f5819d3acc0ce3a0670d282c014f972
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-12-01 22:23:54 +0500

    1

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -5,7 +5,7 @@ import { HiArrowRight } from 'react-icons/hi2'
 
 const ProjectsBtn = () => {
   return (
-    <div className="mx-auto xl:mx-0">
+    <div className="mx-auto xl:mx-0 z-50">
       <Link href={'/work'}
         className="relative w-[105px] h-[105px] flex items-center justify-center bg-circleStar bg-cover bg-center bg-no-repeat group">
         <Image src={'/rounded-text.png'} width={111} height={118} alt=""
